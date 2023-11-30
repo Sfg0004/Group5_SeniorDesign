2bbdcd55760da564f788f1463fcbe4c9e54cb419
Sfg0004 <100049675+Sfg0004@users.noreply.github.com>
2023-11-30 15:39:23 -0800

    commented Modified_SimpleBlockchain.py

--- Modified_SimpleBlockchain.py
+++ Modified_SimpleBlockchain.py
@@ -62,18 +62,20 @@ class Blockchain:
 	#	new_proof = 1
 	#	check_proof = False
 
+	#function for hashing
 	def hash(self, block):
 		encoded_block = json.dumps(block, sort_keys=True).encode()
 		return hashlib.sha256(encoded_block).hexdigest()
 
+	#function to check the blockchain's validity
 	def chain_valid(self, chain):
 		previous_block = chain[0]
 		block_index = 1
-
+-		
 		while block_index < len(chain):
 			block = chain[block_index]
 			if block['previous_hash'] != self.hash(previous_block):
-				return False
+				return False	#the hashes do not line up so the blockchain is invalid
 
 			previous_proof = previous_block['proof']
 			proof = block['proof']
@@ -81,7 +83,7 @@ class Blockchain:
 				str(proof**2 - previous_proof**2).encode()).hexdigest()
 
 			if hash_operation[:5] != '00000':
-				return False
+				return False	#if the block wasn't created correctly with consensus mechanism blockchain is invalid
 			previous_block = block
 			block_index += 1
 
@@ -92,15 +94,14 @@ class Blockchain:
 blockchain = Blockchain()
 
 # Mining a new block
-
-
 def mine_block():
 	previous_block = blockchain.print_previous_block()
-	previous_proof = previous_block['proof']
-	proof = blockchain.proof_of_work(previous_proof)
-	previous_hash = blockchain.hash(previous_block)
-	block = blockchain.create_block(proof, previous_hash)
+	previous_proof = previous_block['proof']	#gets previous block's proof
+	proof = blockchain.proof_of_work(previous_proof)	#mines with new proof of work
+	previous_hash = blockchain.hash(previous_block)		#gets previous block's hash to store
+	block = blockchain.create_block(proof, previous_hash)	#creates block with all the data required
 
+	#output for user to see that the block is mined and info about block
 	response = {'message': 'A block is MINED',
 				'index': block['index'],
 				'timestamp': block['timestamp'],
@@ -108,7 +109,7 @@ def mine_block():
 				'previous_hash': block['previous_hash']}
 
 	i = 1
-	for key, value in response.items():
+	for key, value in response.items():	#output layout for printing the message to terminal
 		print(f"> {key} : {value}")
 		i += 1
 	#print(f"{response}\n")
@@ -125,16 +126,17 @@ def display_chain():
 	#print(f"{response}\n")
 
 # Check validity of blockchain
+#calls to chain_valid to check
 def valid():
-	valid = blockchain.chain_valid(blockchain.chain)
+	valid = blockchain.chain_valid(blockchain.chain)	#var holds results of chain_valid function
 
 	if valid:
-		response = {'message': 'The Blockchain is valid.'}
+		response = {'message': 'The Blockchain is valid.'}	#outputs to user in terminal
 	else:
-		response = {'message': 'The Blockchain is not valid.'}
+		response = {'message': 'The Blockchain is not valid.'}	#outputs to suer in terminal
 	
 	i = 1
-	for key, value in response.items():
+	for key, value in response.items():	#output display format
 		print(f"> {key} : {value}\n")
 		i += 1
 
@@ -145,14 +147,15 @@ def valid():
 
 #Runs IPFS program
 def ipfs():
-	os.system("node index.js > file.txt")
-	with open('file.txt') as file:
+	os.system("node index.js > file.txt")	#runs the command to begin IPFS code and puts output into file for parsing later
+	with open('file.txt') as file:	#reads the file
 		lines = file.readlines()
 	path = lines[2]
-	parsedPath = path.split('/')
-	print(f"File successfully added to IPFS: {path}")
-	return parsedPath[4]
+	parsedPath = path.split('/')	#splits up the file text to get only the needed section (hash)
+	print(f"File successfully added to IPFS: {path}")	#lets user know the file has be successfully uploaded
+	return parsedPath[4]			#retrieves hash
 
+#function for printing the menu for the user each time
 def printMenu():
 	print("What would you like to do concerning your blockchain?:")
 	print("[1] Mine a Block")
@@ -162,25 +165,26 @@ def printMenu():
 
 # Run program
 def main():
-	printMenu()
-	choice = input("Choose 1-4 or q to quit: ")
+	printMenu()	#prints menu for user to select
+	choice = input("Choose 1-4 or q to quit: ")	#takes user input
 	print()
 	while choice != 'q':
-		if choice == 'q':
+		if choice == 'q':	#quits program
 			break
 		elif choice == "1":
-			mine_block()
+			mine_block()	#mines block
 		elif choice == "2":
-			display_chain()
+			display_chain()	#shows the current blockchain
 		elif choice == "3":
-			valid()
+			valid()		#checks blockchain validity
 		elif choice == "4":
-			hash = ipfs()
+			hash = ipfs()	#gets file hash for later use
 
 		print()
-		printMenu()
-		choice = input("Choose 1-4 or q to quit: ")
+		printMenu()	#prints menu again
+		choice = input("Choose 1-4 or q to quit: ") #takes user input
 		print()
 
+#function to run main
 if __name__ == "__main__":
 	main()
